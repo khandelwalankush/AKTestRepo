61572cc4a09d51dcf9d72c4143af2b2a256683e2
Ankush Khandelwal <ankush143@gmail.com>
2019-02-01 21:05:04 +0530

    new files

---
+++ src/com/ak/testgit/main/TestGitMainClass.java
@@ -0,0 +1,9 @@
+package com.ak.testgit.main;
+
+public class TestGitMainClass {
+
+	public static void main(String[] args) {
+		System.out.println("Git is fun1");
+		System.out.println("Git is fun and cool1");
+	}
+}
