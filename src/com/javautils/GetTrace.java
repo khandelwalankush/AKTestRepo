e1ca147bb951183c786dd26f9e5ca417aa3dc9b6
Ankush Khandelwal <Ankush.Khandelwal@blujaysolutions.com>
2020-01-08 17:05:26 +0530

    commit

---
+++ src/com/javautils/GetTrace.java
@@ -0,0 +1,44 @@
+package com.javautils;
+import java.io.*;
+
+// Java program to search exceptions in a huge log file on the server
+public class GetTrace {
+
+	public static void main(String args[]) {
+
+	try {
+		// input file
+		FileInputStream fstream = new FileInputStream("D:\\Work\\logs\\stderrlastline.log");
+
+		DataInputStream in = new DataInputStream(fstream);
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+		// output file
+		BufferedWriter out = new BufferedWriter(new FileWriter("out.txt"));
+
+		String strLine;
+		Boolean broken = false;
+		int line = 0;
+
+		while ((strLine = br.readLine()) != null) {
+			if (strLine.contains("Exception")){		// keyword
+				broken = true;
+			}
+			if (broken) {
+				out.write(strLine);
+				out.newLine();
+				line++;
+			}
+			if (line == 10) {	// print next 10 lines after exception
+				broken = false;
+				line = 0;
+			}
+		}
+
+		in.close();
+		out.close();
+		System.out.println("Successfully written to file");
+	} catch (Exception e) {
+		System.err.println("Error: " + e.getMessage());
+	}
+}}
